ee8b10b98a398bc6ce4688b975864f7bf2ce704c
Charles Neau <charles63500@gmail.com>
2018-06-20 23:04:23 +0100

    keyboard arrows / wasd

--- static/index.js
+++ static/index.js
@@ -1,7 +1,6 @@
-let kelly = ["#e6194b", "#3cb44b", "#ffe119", "#0082c8", "#f58231", "#911eb4", "#46f0f0", "#f032e6", "#d2f53c", "#fabebe", "#008080", "#e6beff", "#aa6e28", "#fffac8", "#800000", "#aaffc3", "#808000", "#ffd8b1", "#000080", "#808080", "#FFFFFF", "#000000"];
-let lineWidths = [5, 10, 20, 40, 80, 160];
-
 window.onload = () => {
+    let kelly = ["#e6194b", "#3cb44b", "#ffe119", "#0082c8", "#f58231", "#911eb4", "#46f0f0", "#f032e6", "#d2f53c", "#fabebe", "#008080", "#e6beff", "#aa6e28", "#fffac8", "#800000", "#aaffc3", "#808000", "#ffd8b1", "#000080", "#808080", "#FFFFFF", "#000000"];
+    let lineWidths = [5, 10, 20, 40, 80, 160, 300, 500, 1000];
     let canvas = document.getElementById('canvas');
     let legend = document.getElementById('legend');
     let ctx = canvas.getContext("2d");
@@ -11,6 +10,8 @@ window.onload = () => {
     let color = 0;
     let indexLineWidth = 1;
     var previous = null;
+
+
     socket.onmessage = (event) => {
         let msg = JSON.parse(event.data);
         ctx.strokeStyle = kelly[msg.c];
@@ -21,6 +22,8 @@ window.onload = () => {
         ctx.lineTo(msg.s[2], msg.s[3]);
         ctx.stroke();
     };
+
+
     canvas.onmousemove = (event) => {
         if (event.which == 1) {
             if (previous != null) {
@@ -31,12 +34,11 @@ window.onload = () => {
         }
         previous = null;
     };
+
+
     canvas.onmouseup = (event) => {
         if (event.which == 3) {
             ++indexLineWidth;
-            if (indexLineWidth > lineWidths.length - 1) {
-                indexLineWidth = 0;
-            }
         }
         if (event.which == 2) {
             --indexLineWidth;
@@ -44,25 +46,65 @@ window.onload = () => {
                 indexLineWidth = lineWidths.length - 1;
             }
         }
+        indexLineWidth = indexLineWidth % lineWidths.length;
         legend.innerHTML = lineWidths[indexLineWidth];
+        legend.style = `color: ${kelly[color]};`;
     };
+
+
     canvas.oncontextmenu = (event) => {
         event.preventDefault();
     };
+
+
     canvas.onmousewheel = (event) => {
+        event.preventDefault();
         if (event.deltaY < 0) {
+            ++color;
+        } else {
             --color;
             if (color < 0) {
                 color = kelly.length - 1;
             }
-        } else {
-            ++color;
         }
         color = color % kelly.length;
-        event.preventDefault();
         legend.innerHTML = lineWidths[indexLineWidth];
         legend.style = `color: ${kelly[color]};`;
     };
+
+    document.onkeydown = function (evt) {
+        switch (evt.keyCode) {
+            case 87: // w
+            case 38: // up
+                ++color;
+                break;
+            case 83: // s
+            case 40: // down
+                --color;
+                if (color < 0) {
+                    color = kelly.length - 1;
+                }
+                break;
+            case 65: // a
+            case 37: // left
+                --indexLineWidth;
+                if (indexLineWidth < 0) {
+                    indexLineWidth = lineWidths.length - 1;
+                }
+                break;
+            case 68: // d
+            case 39: // right
+                ++indexLineWidth
+                break;
+            default:
+                break;
+        }
+        indexLineWidth = indexLineWidth % lineWidths.length;
+        color = color % kelly.length;
+        legend.innerHTML = lineWidths[indexLineWidth];
+        legend.style = `color: ${kelly[color]};`;
+    };
+
     legend.innerHTML = "use your mouse to draw";
     legend.style = `color: ${kelly[color]};`;
 }
