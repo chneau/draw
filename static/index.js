3c92a770a5aa2acc17a52a5dcc9f60f187020102
Charles Neau <charles63500@gmail.com>
2018-06-23 13:04:13 +0100

    new linewidths

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     let kelly = ["#e6194b", "#3cb44b", "#ffe119", "#0082c8", "#f58231", "#911eb4", "#46f0f0", "#f032e6", "#d2f53c", "#fabebe", "#008080", "#e6beff", "#aa6e28", "#fffac8", "#800000", "#aaffc3", "#808000", "#ffd8b1", "#000080", "#808080", "#FFFFFF", "#000000"];
-    let lineWidths = [5, 10, 20, 40, 80, 160, 300, 500, 1000, 10000];
+    let lineWidths = [2, 5, 10, 20, 40, 80, 160, 320, 640];
     let canvas = document.getElementById('canvas');
     let legend = document.getElementById('legend');
     let ctx = canvas.getContext("2d");
